a22f597e8ba7fdd6978a22eb477685d5cdc10c85
SiamV <avers3452@gmail.com>
2023-07-24 22:08:56 -0500

    create structure site

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,28 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `site`,
+    title: `Мототуры по Мексике`,
+    description: `Организация мототуров и мотопутешествий по Мексике и Латинской Америке`,
     siteUrl: `https://la-moto.ru`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
     },
-    __key: "images"
-  }]
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }
+  ]
 };
